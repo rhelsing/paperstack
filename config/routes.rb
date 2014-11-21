b5695bd87991287d1a4eae90dd8765042f1c8512
Ryan Helsing <ryanhelsing@gmail.com>
2014-11-21 10:41:03 -0500

    sample templates to style

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/dashboard'
+
   resources :boxes
 
   # The priority is based upon order of creation: first created -> highest priority.
